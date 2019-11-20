e1318295d2a5cf6a4b50c5fd247c50b075b6b38d
elvisproject <elvistasung99@gmail.com>
2019-11-20 10:08:16 +0700

    projek pemograman web

---
+++ On-member/index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+if ( !isset($_SESSION['user_login']) || 
+    ( isset($_SESSION['user_login']) && $_SESSION['user_login'] != 'member' ) ) {
+	header('location:./../login.php');
+	exit();
+}
+?>
+<h2>Hallo Member <?=$_SESSION['nama'];?> Apakabar ?</h2>
+
+<a href="./../logout.php">Logout</a>
